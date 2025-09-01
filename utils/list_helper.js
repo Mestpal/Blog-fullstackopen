8f9cc7117b4aa1b5b04c117c5536471783669f91
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-09-01 14:49:29 +0200

    Exercise 4.4 Blog 4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likesSum = (result, blog) => {
+    return blog?.likes
+      ? result + blog.likes
+      : result
+  }
+  return blogs.reduce(likesSum,0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
