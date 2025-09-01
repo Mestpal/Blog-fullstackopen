94f26d04257a44542dd0da1a9536324e389f9763
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-09-01 14:04:47 +0200

    exercise 4.1 blog 1

---
+++ App.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,6 @@
+const app  = require('./App')
+const config = require('./utils/config')
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
