c4f2ad4eb64acd6049a991c6c7f35e3839429dd7
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-09-10 13:47:58 +0200

    Exercise 4.20 Blog 20

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ const mongoose = require('mongoose')
 
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const loginRouter = require('./controllers/log')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/login', loginRouter)
